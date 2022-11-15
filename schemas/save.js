f0a3b082de4003d12a7246bd1c9ba1d499326327
marie <marielee14@gmail.com>
2022-11-15 09:47:25 +0900

    backend-fields updated(1)

---
+++ schemas/save.js
@@ -0,0 +1,9 @@
+export default {
+  name: "save",
+  title: "save",
+  type: "document",
+  fields: [
+    { name: "postedBy", title: "PostedBy", type: "postedBy" },
+    { name: "useId", title: "UserId", type: "string" },
+  ],
+};
